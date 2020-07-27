0098ff175b154e9a26c7d4d5a1dd4abbd85d3359
Souphiane Bensalim <souphianebensalim@gmail.com>
2020-07-27 18:31:54 +0200

    Updates

---
+++ precache-manifest.d696f5be82fc107fff9361e275eea045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493f460d25039fc340f3d8e813fe4c95",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cd13b2f83a64da87b1ef",
+    "url": "/monsters-rolodex/static/css/main.c817aa78.chunk.css"
+  },
+  {
+    "revision": "77d19fc2296beccfe3af",
+    "url": "/monsters-rolodex/static/js/2.5ad77bcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.5ad77bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd13b2f83a64da87b1ef",
+    "url": "/monsters-rolodex/static/js/main.3963f125.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
